4f8875cfaf91635639bb9044022b303851aa75e8
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-11-11 00:04:52 +0800

    Object css

--- app/src/components/MyFavorite/Object/addEvent.vue
+++ app/src/components/MyFavorite/Object/addEvent.vue
@@ -6,10 +6,24 @@
 
 <script>
 export default {
-name:'addEvent'
+  name: 'addEvent'
 }
 </script>
 
-<style>
+<style scoped>
+.addEvent input {
+  width: 300px;
+  height: 18px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+  margin: 2px 20px;
+}
 
+.addEvent input :focus {
+  outline: none;
+  border-color: rgb(138, 241, 244, 0.8);
+  box-shadow: inset 0 1px 1px rgb(0, 0, 0, 0.075), 0 0 8px rgb(138, 241, 244, 0.8);
+}
 </style>
